e967adfbb905c3f2a8e86baae2099fcba18c2001
Nicolas <nick_.s@hotmail.com>
2020-10-18 19:03:05 -0300

    aula de java script, date e lacos de repeticao

--- js/main.js
+++ js/main.js
@@ -1,11 +1,40 @@
+
+var d = new Date;
+console.log("Hoje é " + d.getDate() + "/" + (d.getMonth()+1));
+alert("Hoje é " + d.getDate() + "/" + (d.getMonth()+1));
+
+// Condicionais, laçõs de repetição e Date
+
+// var idade = prompt("Qual sua idade?")
+// if (idade >= 18){
+//     alert("maior de idade")
+// }else{
+//     alert("menor de idade")
+// };
+
+// var count = 0;
+// while (count <= 5){
+//     console.log(count);
+//     count++;
+// }
+
+// var d = new Date();
+// alert(d);
+// alert(d.getMonth());
+// alert(d.getMinutes());
+// alert(d.getDay())
+// alert(d.getHours())
+
+
+
 // Arrays e Dicionário - AULA 2
 
 
-/*lista de dicionarios*/
+/*   ******* lista de dicionarios   ***************/
 
-var frutas = [{nome:"maça", cor: "vermelha"},{nome:"uva",cor: "roxa"}];
-console.log(frutas);
-alert(frutas[1].nome);
+// var frutas = [{nome:"maça", cor: "vermelha"},{nome:"uva",cor: "roxa"}];
+// console.log(frutas);
+// alert(frutas[1].nome);
 
 /* ****dicionário****
 var fruta = {nome:"maça", cor: "vermelha"}
@@ -13,14 +42,14 @@ console.log(fruta.nome);
 alert(fruta.cor);
 */
 
-var lista = ["maça","pera","laranja"];
-lista.push("uva");
-//lista.pop();
-console.log(lista);
-console.log(lista.lenght);
-console.log(lista.reverse());
-console.log(lista.toString());
-console.log(lista.join(" - "))
+// var lista = ["maça","pera","laranja"];
+// lista.push("uva");
+// //lista.pop();
+// console.log(lista);
+// console.log(lista.lenght);
+// console.log(lista.reverse());
+// console.log(lista.toString());
+// console.log(lista.join(" - "))
 
 // Introdução AULA 1
 
