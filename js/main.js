2730b05f139229bca41c020f5188769ab0719fe8
Nicolas <nick_.s@hotmail.com>
2020-10-18 18:17:58 -0300

    aulas de javascript, introducao, arrays e dicionario

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// Arrays e Dicionário - AULA 2
+
+
+/*lista de dicionarios*/
+
+var frutas = [{nome:"maça", cor: "vermelha"},{nome:"uva",cor: "roxa"}];
+console.log(frutas);
+alert(frutas[1].nome);
+
+/* ****dicionário****
+var fruta = {nome:"maça", cor: "vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+var lista = ["maça","pera","laranja"];
+lista.push("uva");
+//lista.pop();
+console.log(lista);
+console.log(lista.lenght);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(" - "))
+
+// Introdução AULA 1
+
+
+// var nome = "Nicolas Vogiantzis";
+// var idade = 23;
+// var idade2 = 10;
+// var frase = "Japão é o melhor time do mundo"
+// //alert("Bem Vindo " + nome + idade);
+// console.log(nome);
+// console.log(idade + idade2);
+// console.log(frase.replace("Japão", "Brasil"));
+// alert(frase.replace("Japão","Brasil"));
+
