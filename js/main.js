56c71120199c4b48083842c31a7124be5b9b0bb8
Nicolas <nick_.s@hotmail.com>
2020-10-25 13:35:35 -0300

    aulas de javascript da digital innovation, sobre functions, buttons

--- js/main.js
+++ js/main.js
@@ -1,9 +1,82 @@
+// // ******************* AULA 5 - Manipulando elementos
 
-var d = new Date;
-console.log("Hoje é " + d.getDate() + "/" + (d.getMonth()+1));
-alert("Hoje é " + d.getDate() + "/" + (d.getMonth()+1));
+function clicou(){
+   alert("Obrigado por clicar");
+}
 
-// Condicionais, laçõs de repetição e Date
+function clicou2(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+}
+
+function redirecionar(){
+        //window.open("http://www.google.com.br");
+        window.location.href = "https://www.google.com.br/";
+}
+
+function trocar(elemento){
+    elemento.innerHTML = "Obrigado por passar o mouse";
+
+}
+
+function voltar(elemento){
+    elemento.innerHTML = "Passe o mouse aqui";
+
+}  
+
+function load(){
+    alert("Página carregada");
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value) // coleta os valores 
+}
+
+
+// function trocar(){
+//         document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+
+// }
+
+// function voltar(){
+//         document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+
+// }  
+
+// ******************* AULA 4 - Funções 
+
+
+/* var validar = 0;
+function validaIdade(idade){
+    if (idade >= 18){   
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+
+    var idade = prompt("Qual sua idade");
+    validaIdade(idade);
+    console.log(validar);
+*/
+
+// function soma(n1,n2){
+//     return n1 + n2;
+// }
+
+// function setReplace(frase,nome, novo_nome){
+//     return frase.replace(nome, novo_nome)
+// }
+// alert(soma(5,10));
+// alert(setReplace("Vai Japão", "Japão","Brasil"));
+
+
+// var d = new Date;
+// console.log("Hoje é " + d.getDate() + "/" + (d.getMonth()+1));
+// alert("Hoje é " + d.getDate() + "/" + (d.getMonth()+1));
+
+// ******************* AULA 3 - Condicionais, laçõs de repetição e Date  
+
 
 // var idade = prompt("Qual sua idade?")
 // if (idade >= 18){
@@ -27,10 +100,7 @@ alert("Hoje é " + d.getDate() + "/" + (d.getMonth()+1));
 
 
 
-// Arrays e Dicionário - AULA 2
-
-
-/*   ******* lista de dicionarios   ***************/
+// ******************* Arrays e Dicionário - AULA 2 ****************
 
 // var frutas = [{nome:"maça", cor: "vermelha"},{nome:"uva",cor: "roxa"}];
 // console.log(frutas);
@@ -51,7 +121,7 @@ alert(fruta.cor);
 // console.log(lista.toString());
 // console.log(lista.join(" - "))
 
-// Introdução AULA 1
+// *******************  Introdução AULA 1
 
 
 // var nome = "Nicolas Vogiantzis";
